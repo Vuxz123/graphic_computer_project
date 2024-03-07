176b5d075046ae864bf32cadcbf87a3979e5ad01
Vuxz123 <ethnicthv123@gmail.com>
2024-03-07 12:27:13 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+import {GLTFLoader} from "three/addons";
+
+const scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+//set the background color of the scene
+renderer.setClearColor(0x000000);
+
+document.body.appendChild(renderer.domElement);
+
+const loader = new GLTFLoader();
+let mixer;
+loader.load('/static/scene/dancing.glb', function (gltf) {
+    mixer = new THREE.AnimationMixer(gltf.scene);
+    scene.add(gltf.scene);
+    camera = gltf.cameras[0];
+    const action = mixer.clipAction(gltf.animations[2]);
+    action.play();
+}, undefined, function (error) {
+    console.error(error);
+});
+
+const clock = new THREE.Clock();
+
+function animate() {
+    if (mixer) mixer.update(clock.getDelta());
+    if (camera) {
+        renderer.render(scene, camera);
+    }
+}
+
+window.onresize = function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+renderer.setAnimationLoop(animate);
